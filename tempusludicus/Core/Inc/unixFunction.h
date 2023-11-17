86754e803a3244edc76c1e794d023fbe68cc830e
drankhek <roelvaneeten@hotmail.nl>
2023-11-18 00:43:44 +0100

    add display function and unix function
    C and H file added with given function for unix
    add display function
    
    clock for backlight don't work. Need to see for that.

---
+++ tempusludicus/Core/Inc/unixFunction.h
@@ -0,0 +1,22 @@
+#ifndef UNIXFUNCTION_H
+#define UNIXFUNCTION_H
+
+#include <MKL25Z4.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct
+{
+ uint16_t year;
+ uint16_t month;
+ uint16_t day;
+ uint16_t hour;
+ uint16_t minute;
+ uint8_t second;
+ 
+}datetime_t;
+
+void RTC_HAL_ConvertSecsToDatetime(const uint32_t * seconds, datetime_t * datetime);
+void RTC_HAL_ConvertDatetimeToSecs(const datetime_t * datetime, uint32_t * seconds);
+
+#endif
--- tempusludicus/Core/Src/main.c
+++ tempusludicus/Core/Src/main.c
@@ -13,12 +13,27 @@
 #include "delay.h"
 #include "rgb.h"
 #include "sysTick.h"
+#include "lcd_4bit.h"
+#include "unixFunction.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <MKL25Z4.h>
 
 int main()
 {
     init_rgb();
+		
+		lcd_init();
 
     init_sysTick();
+		
+		uint32_t unixtest = 17001550850;
+		datetime_t unixTime;
+		
+		lcd_clear();
+    lcd_bl_pwmcontrol(0x2000);
 
     set_rgb(1, 0, 0);
     delay_us(1000000);
@@ -29,5 +44,12 @@ int main()
     __enable_irq();
 
     while (1) {
+		RTC_HAL_ConvertDatetimeToSecs(&unixTime,&unixtest);
+		
+		lcd_putdatetime(unixTime);
+		
+
+		
+		
     }
 }
